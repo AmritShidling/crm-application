f13fedb682bbf0fc99daa3ca41e86aa08d683eb8
AmritShidling <amritlshidling@gmail.com>
2025-11-17 19:15:41 +0000

    Lead microservice

---
+++ crm-application/crm-lead-service/src/main/java/com/lead/service/crm_lead_service/model/LeadStatus.java
@@ -0,0 +1,8 @@
+package com.lead.service.crm_lead_service.model;
+
+public enum LeadStatus {
+    NEW,
+    CONTACTED,
+    QUALIFIED,
+    CONVERTED
+}
---
+++ crm-application/crm-lead-service/src/test/java/com/lead/service/crm_lead_service/CrmLeadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lead.service.crm_lead_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmLeadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
